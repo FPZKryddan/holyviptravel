4de07dbf7a94c5ea8e999fc17f6ac21cc9a8746d
Dennis Andersson <dennis.andersson106@gmail.com>
2025-10-08 17:46:02 +0200

    fix image paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       className="absolute top-6 left-1/2 z-10 flex w-full -translate-x-1/2 flex-row gap-12 rounded-4xl border border-white/20 bg-white/30 px-12 py-2 shadow-[0_8px_30px_rgb(0,0,0,0.12)] backdrop-blur-xl md:w-fit"
       style={{ viewTransitionName: 'nav' }}
     >
-      <img src="logo.png" className="h-12" />
+      <img src="/logo.png" className="h-12" />
       <ul className="ml-auto flex flex-row items-center gap-6 px-6">
         <NavItem label="Hem" path="/" />
         <NavItem label="Tävlingsvilkor" path="/tavling" />
